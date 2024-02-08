8c1f665e6c7578ef2159d3558c7ad9a327a07617
Hanagami3 <145351834+Hanagami3@users.noreply.github.com>
2024-02-08 15:54:39 +0100

    Delete Alexandria2 directory

--- Alexandria2/src/main/java/be/intecbrussel/model/book/ChildrenBooks.java
+++
@@ -1,9 +0,0 @@
-package be.intecbrussel.model.book;
-
-
-public class ChildrenBooks extends Book{
-    public ChildrenBooks(Integer bookIdNo , String bookTitle, String bookAuthor , int bookPublishYear, boolean bookInLibrary, boolean bookReserved)
-    {
-        super(bookIdNo,bookTitle,bookAuthor,bookPublishYear,bookInLibrary, bookReserved);
-    }
-}
--- Alexandria2/src/main/java/be/intecbrussel/model/book/FictionBooks.java
+++
@@ -1,9 +0,0 @@
-package be.intecbrussel.model.book;
-
-
-public class FictionBooks extends Book{
-    public FictionBooks(Integer bookIdNo, String bookTitle, String bookAuthor, int bookPublishYear, boolean bookInLibrary, boolean bookReserved) {
-        super(bookIdNo, bookTitle, bookAuthor, bookPublishYear, bookInLibrary, bookReserved);
-    }
-}
-
