b0c4960cfac48f9279856ed2a31f8c046aa1e942
Hanagami3 <145351834+Hanagami3@users.noreply.github.com>
2024-02-12 00:18:20 +0100

    Delete Alexandria2 (1)/Alexandria2 directory

--- Alexandria2 (1)/Alexandria2/src/main/java/be/intecbrussel/model/book/FictionBooks.java
+++
@@ -1,9 +0,0 @@
-package be.intecbrussel.model.book;
-
-
-public class FictionBooks extends Book{
-    public FictionBooks(String bookTitle, String bookAuthor, int bookPublishYear, BookStatus status) {
-        super(bookTitle, bookAuthor, bookPublishYear, status);
-    }
-}
-
