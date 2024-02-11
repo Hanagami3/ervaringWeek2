df8e799e04672f9e5a7cb362ab429224128ca03f
Hanagami3 <145351834+Hanagami3@users.noreply.github.com>
2024-02-11 14:54:51 +0100

    Delete Alexandria2 (1)/Alexandria2 directory

--- Alexandria2 (1)/Alexandria2/src/main/java/be/intecbrussel/model/book/BookStatus.java
+++
@@ -1,9 +0,0 @@
-package be.intecbrussel.model.book;
-
-public enum BookStatus {
-    AVAILABLE,
-    BORROWED,
-    ISSUED,
-    UNAVAILABLE,
-    RESERVED
-}
--- Alexandria2 (1)/Alexandria2/src/main/java/be/intecbrussel/model/book/FindAndSortKeys.java
+++
@@ -1,7 +0,0 @@
-package be.intecbrussel.model.book;
-
-
-public enum FindAndSortKeys {
-    AUTHOR,TITLE,ID, PUBLISHING_YEAR;
-}
-
